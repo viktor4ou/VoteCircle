7d11a14748336b6919016ea2824972b51119222c
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2025-05-04 21:34:59 +0300

    -Fix old commits

--- Client/src/utility/Authentication.js
+++ Client/src/utility/Authentication.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const baseUrl = import.meta.env.VITE_API_BASE_URL;
 //test asdf asf
 export const login = async (obj) => {
@@ -52,58 +51,3 @@ export const logout = async () => {
     });
     localStorage.removeItem("jwtToken");
 };
-=======
-const baseUrl = import.meta.env.VITE_API_BASE_URL;
-
-export const login = async (obj) => {
-    const response = await fetch(`${baseUrl}/Authentication/SignIn`, {
-        method: "POST",
-        credentials: "include",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email: obj.email,
-            password: obj.password,
-        }),
-    });
-
-    const responseData = await response.json();
-    console.log(responseData.token);
-
-    localStorage.setItem("jwtToken", responseData.token);
-    //returns if the status is 200 or 401
-    const isAuthenticated = response.status == 200;
-    return isAuthenticated;
-};
-
-export const register = async (obj) => {
-    const response = await fetch(`${baseUrl}/Identity/register`, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email: obj.email,
-            password: obj.password,
-        }),
-    });
-
-    const isSuccessful = response.status == 200;
-    return isSuccessful;
-};
-
-export const logout = async () => {
-    await fetch(`${baseUrl}/Logout`, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({}),
-    });
-    localStorage.removeItem("jwtToken");
-};
->>>>>>> a78b820 (Apply latest client utility updates)
