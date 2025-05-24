3ab8df47a30bc1e76a1e573cbcb92076b34d6645
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2025-05-24 11:22:22 +0300

    Remove: old authentication handling

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,22 +1,12 @@
 import "../App.css";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router";
+import { Link } from "react-router";
 import { Button } from "./ui/button";
 import { ThemeSwitch } from "@/components/ui/ThemeSwitch";
 import { HamburgerMenu } from "./HamburgerMenu";
-import { logout } from "@/utility/Authentication";
 import { AuthenticationButtons } from "./AuthenticationButtons";
 const Header = () => {
     const [themeSwitchStatus, setThemeSwitchStatus] = useState(false);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const navigate = useNavigate();
-
-    async function logoutOnClick() {
-        const response = await logout();
-        setIsAuthenticated(false);
-        console.log(isAuthenticated);
-        navigate("/");
-    }
 
     return (
         <header className="flex items-center justify-between bg-[#FAFAFA] dark:bg-black h-23 drop-shadow-xl border-b-2 ">
@@ -41,19 +31,7 @@ const Header = () => {
             </div>
 
             <div className="drop-shadow-xl absolute right-1 transform -translate-x-1 max-md:hidden">
-                {isAuthenticated ? (
-                    <Button
-                        className="bg-[#4199FF] drop-shadow-xl me-5 hover:bg-[#357dd0]"
-                        onClick={() => logoutOnClick()}
-                    >
-                        Log out
-                    </Button>
-                ) : (
-                    <AuthenticationButtons
-                        setIsAuthenticated={setIsAuthenticated}
-                        isAuthenticated={isAuthenticated}
-                    />
-                )}
+                <AuthenticationButtons />
             </div>
         </header>
     );
